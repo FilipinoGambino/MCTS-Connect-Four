038e6f31cc31edd5acb11e4ae6c843afeed81203
FilipinoGambino <ngorichs1@gmail.com>
2024-04-03 00:03:22 -0500

    help

--- c4_game/mcts_game.py
+++ c4_game/mcts_game.py
@@ -1,5 +1,7 @@
 from game import Game
 
+# https://github.com/Zeta36/chess-alpha-zero/blob/master/src/chess_zero/agent/player_chess.py#L187
+
 class MCTSGame(Game):
     def __init__(self):
         super().__init__()
